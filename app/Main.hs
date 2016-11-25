6ea72d062cecc7bee74c0bbe04c5cae86d459cb7
dcastro <dcastro@navinet.net>
2016-11-25 12:25:56 +0000

    Added flag parsing, and invocation of `csi`

--- app/Main.hs
+++ app/Main.hs
@@ -1,60 +1,43 @@
 module Main where
 
-import Lib
-import System.Environment
 import Control.Monad
 import Data.List
 import System.Directory
 import System.FilePath
 import System.Process
 import Control.Conditional
-import System.Exit hiding (die)
 
-import Data.Char
-import System.Console.GetOpt
-import System.IO
-import Text.Printf
+import Options.Applicative
 
-data Flags = Asm {asm :: String} | Using { ns :: String } | AsmDir { dir:: String } | Help
-  deriving (Eq,Ord,Show)
-
-flags =
-  [
-    Option ['a'] ["asm"]    (ReqArg Asm "ASSEMBLY")           "Import the specified assembly"
-  , Option ['d'] ["dir"]    (ReqArg AsmDir "ASSEMBLY-FOLDER") "Import all assemblies from the specified folder"
-  , Option ['u'] ["using"]  (ReqArg Using "USING")            "Import the specified namespace"
-  , Option ['h'] ["help"]   (NoArg Help)                      "Print this help message"
-  ]
-
-parse args = case getOpt Permute flags args of
-  (args, _, []) -> if Help `elem` args then help
-                      else foldM f ([], []) args
-  (_, _, errs)  -> putErr (join errs) >> putErr (usageInfo header flags) >> exitWith (ExitFailure 1)
-  where
-    header = "Usage: csi-init [options]"
-    help = putErr (usageInfo header flags) >> exitSuccess
-    f (asms, nss) (Using ns)    = return (asms, ns : nss)
-    f (asms, nss) (Asm asm)     = return (asm : asms, nss)
-    f (asms, nss) (AsmDir dir)  = (\xs -> (asms ++ xs, nss)) <$> files
-      where files = filter ((".dll" ==) . takeExtension) <$> listFilesRecursively dir
-
-putErr = hPutStrLn stderr
-
+-- csi-init -d <bin_dir1> -d <bin_dir2> -- /u:<namespace1>
 main :: IO ()
-main = getArgs >>= parseArgs >>= listFilesRecursively >>= void . run . prepareArgs
+main =
+  customExecParser parserPrefs opts >>= prepareCmd >>= callProcess "csi"
+  where
+    prepareCmd (Flags dirs args) = do
+      dlls <- filter ((".dll" ==) . takeExtension) . join <$> mapM listFilesRecursively dirs
+      return $ ("/r:"++) <$> dlls <|> args
 
--- TODO: pass a list of namespaces
-parseArgs ["-v"]  = usage >> exit
-parseArgs [dir]   = return dir
-parseArgs _       = usage >> exit
+data Flags = Flags {
+  asmDirs :: [String],
+  args    :: [String]
+} deriving (Show)
 
-usage   = putStrLn "Usage: csi-init <bin-folder>"
-exit    = exitSuccess
+flags :: Parser Flags
+flags = Flags
+  <$> many ( strOption
+    ( long "dir"
+    <> short 'd'
+    <> metavar "ASSEMBLYDIR"
+    <> help "Import all assemblies from the specified folder"))
+  <*> many (strArgument (metavar "ARGUMENTS..."))
 
-prepareArgs :: t -> t
-prepareArgs dlls = dlls
+opts = info (helper <*> flags)
+  ( fullDesc
+  <> progDesc "All arguments specified after `--` will be passed to `csi`. E.g.: `csi-init -- /u:Newtonsoft.Json`"
+  <> header "Invokes the `csi` C# REPL preloaded with a bunch of assemblies.")
 
-run args = createProcess (proc "csi" args)
+parserPrefs = prefs showHelpOnError
 
 listFilesRecursively :: FilePath -> IO [String]
 listFilesRecursively dir =
