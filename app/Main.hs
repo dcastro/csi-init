51bcd221bfb1ec19113402aafa796131471c751f
dcastro <dc@diogocastro.com>
2017-01-09 16:39:10 +0000

    Added a couple of newtypes for better type safety

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE MultiParamTypeClasses, TypeSynonymInstances, FlexibleInstances #-}
+
 module Main where
 
 import Control.Monad
@@ -7,8 +9,19 @@ import System.FilePath
 import System.Process
 import System.Environment
 import Control.Conditional hiding (when)
-
 import Options.Applicative
+import Control.Newtype
+
+newtype Directory = Directory String
+newtype Argument  = Argument  String
+
+instance Newtype Directory String where
+  pack = Directory
+  unpack (Directory s) = s
+
+instance Newtype Argument String where
+    pack = Argument
+    unpack (Argument s) = s
 
 -- csi-init -d <bin_dir1> -d <bin_dir2> -- /u:<namespace1>
 main :: IO ()
@@ -16,47 +29,47 @@ main = do
   fs <- mkOpts >>= customExecParser parserPrefs
   args <- prepareArgs fs
   printArgs fs args
-  callProcess "csi" args
+  callProcess "csi" (unpack <$> args)
 
-prepareArgs :: Flags -> IO [String]
+prepareArgs :: Flags -> IO [Argument]
 prepareArgs (Flags dirs rdirs args _) = do
   dlls <- join <$> liftM2 (++) (mapM findDlls dirs) (mapM findDllsRecursively rdirs)
-  let importDirs = ("/lib:" ++) <$> (dirs ++ rdirs)
+  let importDirs = over Directory ("/lib:" ++) <$> (dirs ++ rdirs)
   let importDlls = if null dlls then []
-                    else ["/r:" ++ join (intersperse "," dlls)]
+                    else [pack $ "/r:" ++ join (intersperse "," dlls)]
   return $ importDirs ++ importDlls ++ args
 
-printArgs :: Flags -> [String] -> IO ()
-printArgs fs args = when (debug fs) (putStrLn ("Arg count: " ++ show (length args)) >> mapM_ putStrLn args)
+printArgs :: Flags -> [Argument] -> IO ()
+printArgs fs args = when (debug fs) (putStrLn ("Arg count: " ++ show (length args)) >> mapM_ (putStrLn . unpack) args)
 
-findDlls :: FilePath -> IO [FilePath]
-findDlls dir = filterDlls . map (makeRelative dir) <$> (filterM isFile =<< listDirectoryAbs dir)
+findDlls :: Directory -> IO [FilePath]
+findDlls dir = filterDlls . map (makeRelative $ unpack dir) <$> (filterM isFile =<< listDirectoryAbs dir)
 
-findDllsRecursively :: FilePath -> IO [FilePath]
-findDllsRecursively dir = filterDlls . map (makeRelative dir) <$> listFilesRecursively dir
+findDllsRecursively :: Directory -> IO [FilePath]
+findDllsRecursively dir = filterDlls . map (makeRelative $ unpack dir) <$> listFilesRecursively dir
 
 filterDlls = filter ((".dll" ==) . takeExtension)
 
 data Flags = Flags {
-  asmDirs   :: [String],
-  asmRDirs  :: [String],
-  args      :: [String],
+  asmDirs   :: [Directory],
+  asmRDirs  :: [Directory],
+  args      :: [Argument],
   debug     :: Bool
 }
 
 flags :: Parser Flags
 flags = Flags
-  <$> many ( strOption
+  <$> many (pack <$> strOption
     ( long "dir"
     <> short 'd'
     <> metavar "ASSEMBLYDIR"
     <> help "Import all assemblies from the specified folder"))
-  <*> many ( strOption
+  <*> many (pack <$> strOption
     ( long "rdir"
     <> short 'r'
     <> metavar "ASSEMBLYDIR"
     <> help "Import all assemblies from the specified folder and it's subfolders (recursive search)"))
-  <*> many (strArgument (metavar "ARGUMENTS..."))
+  <*> many (pack <$> strArgument (metavar "ARGUMENTS..."))
   <*> switch
     ( long "debug"
     <> help "Print the arguments being passed to `csi`")
@@ -72,17 +85,17 @@ mkOpts = do
 parserPrefs = prefs showHelpOnError
 
 -- returns absolute filepaths for all files inside a directory and its subdirectories
-listFilesRecursively :: FilePath -> IO [FilePath]
+listFilesRecursively :: Directory -> IO [FilePath]
 listFilesRecursively dir = do
-  let go path = ifM (isFile path) (return [path]) (listFilesRecursively path)
+  let go path = ifM (isFile path) (return [path]) (listFilesRecursively $ pack path)
   paths <- listDirectoryAbs dir
   all   <- mapM go paths
   return (join all)
 
 -- returns absolute filepaths for all files and directories inside a directory
 -- adapted from: http://stackoverflow.com/a/8572250/857807
-listDirectoryAbs :: FilePath -> IO [FilePath]
-listDirectoryAbs dir =
+listDirectoryAbs :: Directory -> IO [FilePath]
+listDirectoryAbs (Directory dir) =
   sort <$> (listDirectory dir >>= mapM (canonicalizePath . (dir </>)))
 
 isFile = doesFileExist
