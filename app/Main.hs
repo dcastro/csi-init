dff3cdfbf9e28349d5287f18c6648161e8ec496e
dcastro <dc@diogocastro.com>
2017-01-12 17:37:55 +0000

    Simplified file search using `filemanip`

--- app/Main.hs
+++ app/Main.hs
@@ -3,20 +3,15 @@
 module Main where
 
 import Control.Monad
-import Data.List
-import System.Directory
-import System.FilePath
+import Data.List hiding (find)
 import System.Process
 import System.Environment
-import Control.Conditional hiding (when)
 import Options.Applicative
 import Control.Newtype
+import System.FilePath.Find hiding (Directory)
 
-{-# ANN findDlls            "HLint: ignore Use fmap" #-}
-{-# ANN findDllsRecursively "HLint: ignore Use fmap" #-}
-
-newtype Directory = Directory String
-newtype Argument  = Argument  String
+newtype Directory = Directory String deriving Show
+newtype Argument  = Argument  String deriving Show
 
 instance Newtype Directory String where
   pack = Directory
@@ -29,36 +24,31 @@ instance Newtype Argument String where
 -- csi-init -d <bin_dir1> -d <bin_dir2> -- /u:<namespace1>
 main :: IO ()
 main = do
-  fs <- mkOpts >>= customExecParser parserPrefs
-  args <- prepareArgs fs
-  printArgs fs args
-  callProcess "csi" (unpack <$> args)
+  (Flags dirs rdirs args debug) <- mkOpts >>= customExecParser parserPrefs
+  dlls <- findFiles dirs rdirs isDll
+  let args' = prepareArgs (dirs ++ rdirs) dlls args
+  when debug (printArgs args')
+  callProcess "csi" (unpack <$> args')
 
-prepareArgs :: Flags -> IO [Argument]
-prepareArgs (Flags dirs rdirs args _) = do
-  dlls <- join <$> liftM2 (++) (mapM findDlls dirs) (mapM findDllsRecursively rdirs)
-  let importDirs = over Directory ("/lib:" ++) <$> (dirs ++ rdirs)
-  let importDlls = if null dlls then []
+findFiles :: [Directory] -> [Directory] -> FilterPredicate -> IO [FilePath]
+findFiles dirs rdirs pred =
+  join <$> sequence (
+    (dirs  <&> (find (depth <? 1) pred . unpack)) ++
+    (rdirs <&> (find always       pred . unpack))
+  )
+
+isDll :: FilterPredicate
+isDll = extension ==? ".dll" &&? fileType ==? RegularFile
+
+prepareArgs :: [Directory] -> [FilePath] -> [Argument] -> [Argument]
+prepareArgs dirs dlls args =
+  let importDirs = over Directory ("/lib:" ++) <$> dirs
+      importDlls = if null dlls then []
                     else [pack $ "/r:" ++ join (intersperse "," dlls)]
-  return $ importDirs ++ importDlls ++ args
+  in  importDirs ++ importDlls ++ args
 
-printArgs :: Flags -> [Argument] -> IO ()
-printArgs fs args = when (debug fs) (putStrLn ("Arg count: " ++ show (length args)) >> mapM_ (putStrLn . unpack) args)
-
-findDlls :: Directory -> IO [FilePath]
-findDlls dir =
-  listDirectoryAbs dir
-  >>= filterM isFile
-  >>= return . filterDlls
-  >>= return . map (makeRelative $ unpack dir)
-
-findDllsRecursively :: Directory -> IO [FilePath]
-findDllsRecursively dir =
-  listFilesRecursively dir
-  >>= return . filterDlls
-  >>= return . map (makeRelative $ unpack dir)
-
-filterDlls = filter ((".dll" ==) . takeExtension)
+printArgs :: [Argument] -> IO ()
+printArgs args = putStrLn ("Arg count: " ++ show (length args)) >> mapM_ (putStrLn . unpack) args
 
 data Flags = Flags {
   asmDirs   :: [Directory],
@@ -94,18 +84,7 @@ mkOpts = do
 
 parserPrefs = prefs showHelpOnError
 
--- returns absolute filepaths for all files inside a directory and its subdirectories
-listFilesRecursively :: Directory -> IO [FilePath]
-listFilesRecursively dir = do
-  let go path = ifM (isFile path) (return [path]) (listFilesRecursively $ pack path)
-  paths <- listDirectoryAbs dir
-  all   <- mapM go paths
-  return (join all)
-
--- returns absolute filepaths for all files and directories inside a directory
--- adapted from: http://stackoverflow.com/a/8572250/857807
-listDirectoryAbs :: Directory -> IO [FilePath]
-listDirectoryAbs (Directory dir) =
-  sort <$> (listDirectory dir >>= mapM (canonicalizePath . (dir </>)))
-
-isFile = doesFileExist
+-- flipped fmap
+infixl 1 <&>
+(<&>) :: Functor f => f a -> (a -> b) -> f b
+(<&>) = flip (<$>)
